ea60c9ca5b38d4aca9d14fe4460b28a271220092
thefx3 <fx.pairault@yahoo.fr>
2025-03-12 12:09:39 +0100

    Hidd the ID key, better display

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
 const body = document.querySelector('body');
 const container = document.createElement('div');
+container.style.width = "75%";
+container.style.minWidth = "700px";
+container.style.maxWidth = "1200px";
+container.style.justifySelf = "center";
 body.appendChild(container);
 container.style.display = 'grid';
 container.style.gap = '10px';
@@ -28,15 +32,21 @@ function addBookToLibrary (title, author, pages, read) {
   displayLibrary(myLibrary);
 }
 
-addBookToLibrary("ReadyPlayerOne", "Jack Johnson", 297, "not read yet");
-addBookToLibrary("It", "Stephen King", 498, "read");
-addBookToLibrary("Il nome della rosa", "Fiorantina Alice", 459, "read");
-addBookToLibrary("Millenium", "Ghandi", 2000, "not read yet");
+addBookToLibrary("ReadyPlayerOne", "Jack Johnson", 297, "Not read yet");
+addBookToLibrary("It", "Stephen King", 498, "Read");
+addBookToLibrary("Il nome della rosa", "Fiorantina Alice", 459, "Read");
+addBookToLibrary("Millenium", "Ghandi", 2000, "Not read yet");
 
 
 function createGrid (height, width){
   container.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
-  container.style.gridTemplateRows = `repeat(${height}, 1fr)`;
+  // container.style.gridTemplateRows = `repeat(${height}, 1fr)`;
+
+  let rowHeights = new Array(height).fill("auto");
+  rowHeights[0] = "minmax(0, 50px)";
+  rowHeights[1] = "minmax(0, 50px)";
+
+  container.style.gridTemplateRows = rowHeights.join(" ");
 }
 
 function displayLibrary(array) {  //Grid
@@ -58,6 +68,7 @@ function displayLibrary(array) {  //Grid
     library.style.alignContent = "center";
     library.style.backgroundColor = "darkred";
     library.style.color = "white";
+    library.style.height = "50px";
     container.appendChild(library);
 
     library.style.gridColumn = "1 / span 4";
@@ -65,8 +76,11 @@ function displayLibrary(array) {  //Grid
     const newbook = document.createElement("button");
     newbook.classList.add(".newbook");
     newbook.textContent = "Add New Book";
+    newbook.style.fontWeight = "bold";
     newbook.style.gridColumn = "5 / span 1";
     newbook.style.gridRow = "1";
+
+    newbook.style.height = "50px";
     container.appendChild(newbook);
 
     newbook.addEventListener('click', function() {
@@ -86,20 +100,29 @@ function displayLibrary(array) {  //Grid
       header.style.backgroundColor = "darkblue";
       header.style.color = "white";
       container.appendChild(header);
+      header.style.height = "50px";
     })
    
+
+    
+   
     myLibrary.forEach(book => {
-      Object.values(book).forEach(value => {
+      Object.entries(book).forEach(([key,value]) => {
 
         if (typeof value === "function") return;
 
         const cell = document.createElement("div");
+
+        if (key === "id") { cell.classList.add("id-cell"); }
+
         cell.textContent = value;
         cell.style.border = "1px solid black";
         cell.style.padding = "10px";
         cell.style.textAlign = "center";
         cell.style.alignContent = "center";
         container.appendChild(cell);
+
+        
       });
     });
 
